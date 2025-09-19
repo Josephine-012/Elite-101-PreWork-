3638d51713c37610cef37a120324866a5ba1554b
Josephine-012 <josephineofori122@gmail.com>
2025-09-19 23:24:48 +0000

    Elite PreWork Chatbot with initial requirements

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hello world")
+def greet_user(name):
+    print(f"Hello, {name}!")
+def ask_age(age):
+    print(f"You're, {age}!")
+def ask_help(help):
+    print(f"I can help you with, {help}!")
+def choose_option(option):
+    print(f"I can help you contiue this conversation by, {option}!")
+
+print("Welcome to the Elite 101 Chatbot")
+name=input("Please enter your name: ")
+age=input("Please enter your age: ")
+ask=input("What can I help you with: ")
+choose=input("Choose from this option menu: Placeholder1, Placeholder 2, Placeholder 3: ")
+
+
+greet_user(name)
+ask_age(age)
+ask_help(ask)
+choose_option(choose)
